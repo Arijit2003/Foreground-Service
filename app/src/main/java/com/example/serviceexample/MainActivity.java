135a1195c63146b0c1b93f50a4ae8d9998c36263
Arijit2003 <arijitmodak2003@gmail.com>
2023-03-16 00:05:48 +0530

    First Commit

---
+++ app/src/main/java/com/example/serviceexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.serviceexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start,stop;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        start=findViewById(R.id.start);
+        stop=findViewById(R.id.stop);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this,MusicService.class));
+
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this,MusicService.class));
+            }
+        });
+    }
+}
